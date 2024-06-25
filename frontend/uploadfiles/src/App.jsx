8554582415d968d47850dc08966659650c8fafb9
mairamar <martinezmaira43c1@gmail.com>
2024-06-25 04:14:32 -0300

    cambios-maira

--- frontend/uploadfiles/src/App.jsx
+++ frontend/uploadfiles/src/App.jsx
@@ -1,11 +1,15 @@
 import FileUpload from './components/FileUpload'
 import './App.css'
+import Footer from './Footer'
+import Header from './Header'
 
 const App = () => {
     return (
-        <div className="App">
-            <FileUpload />
-        </div>
+            <div className="App">
+                <Header />
+                <FileUpload />
+                <Footer />
+            </div>
     )
 }
 
---
+++ frontend/uploadfiles/src/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './header.css';
+
+function Header() {
+    return (
+        <header className="header">
+             <div className="logo-container">
+             <img src="Logo.png" alt="Logo" className="logo" />
+            </div>
+            <nav className="nav">
+                <ul className="nav-list">
+                    <li className="nav-item"><a href="#login">Inicio de sesión</a></li>
+                    <li className="nav-item"><a href="#nosotras">Nosotras</a></li>
+                    <li className="nav-item"><a href="http://localhost:5173">Comenzar</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
